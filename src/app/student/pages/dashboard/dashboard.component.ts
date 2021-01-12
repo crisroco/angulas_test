1cca772bc0e11f00a33291888fdc4ac0bf9d9dff
Edmundo <edmundoach@hotmail.com>
2021-01-12 15:09:29 -0500

    removing post classes modal

--- src/app/student/pages/dashboard/dashboard.component.ts
+++ src/app/student/pages/dashboard/dashboard.component.ts
@@ -83,7 +83,7 @@ export class DashboardComponent implements OnInit {
 	ngOnInit() {
 		// this.SurveyModal.open();
 		// this.SurveyModal2.open();
-		this.postEnrollmentModal.open();
+		// this.postEnrollmentModal.open();
 		// this.aficheModal.open();
 		this.studentS.getDataStudent({email: this.user.email})
 		.then(res => {
