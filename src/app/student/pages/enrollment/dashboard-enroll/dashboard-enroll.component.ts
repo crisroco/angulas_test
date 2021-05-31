4c15199145d1d1fb539401af6384d6b608d20d3b
Edmundo <edmundoach@hotmail.com>
2021-05-31 18:32:34 -0500

    fixing maxCredits

--- src/app/student/pages/enrollment/dashboard-enroll/dashboard-enroll.component.ts
+++ src/app/student/pages/enrollment/dashboard-enroll/dashboard-enroll.component.ts
@@ -185,7 +185,7 @@ export class DashboardEnrollComponent implements OnInit {
       this.availableCourses = res.sort((a,b) => {
         return a.UCS_CICLO - b.UCS_CICLO
       });
-      this.maxCredits = Math.round(this.availableCourses[0]['MAX_TOTAL_UNIT']);
+      this.maxCredits = Math.round(this.availableCourses[0]['FT_MAX_TOTAL_UNIT']);
       this.session.setItem('MaxCreditsEnrollment', this.maxCredits)
       this.loadCoursesAlready();
     });
