30f14a1f363038791baf4dc35e4539cba5604f9c
Edmundo <eacostac@cientifica.edu.pe>
2021-08-03 11:44:51 -0500

    moodle strm

--- src/app/student/pages/action/weekly-schedule/weekly-schedule.component.ts
+++ src/app/student/pages/action/weekly-schedule/weekly-schedule.component.ts
@@ -40,7 +40,8 @@ export class WeeklyScheduleComponent implements OnInit {
 	    "CINVE": "https://aulavirtualcpe.cientifica.edu.pe/",
 	    "TITPR": "https://aulavirtualcpe.cientifica.edu.pe/",
 	    "ECONT": "https://aulavirtualcpe.cientifica.edu.pe/",
-	    "PREUN": "https://aulavirtualcpe.cientifica.edu.pe/"
+	    "PREUN": "https://aulavirtualcpe.cientifica.edu.pe/",
+		"NEWSTRM": "https://new-aulavirtualcpe.cientifica.edu.pe/"
 	}
 
 	events:CalendarEvent[] = [];
@@ -202,7 +203,11 @@ export class WeeklyScheduleComponent implements OnInit {
 			var rdate = Math.floor(Date.now() / 1000);
 			emplid = encodeURIComponent(CryptoJS.AES.encrypt(JSON.stringify(this.student.codigoAlumno + '//' + rdate), 'Educad123', {format: this.generalS.formatJsonCrypto}).toString());
 		}
-		window.open(this.virtualRoom[this.realClass.INSTITUTION] + 'local/wseducad/auth/sso.php?strm=' + this.realClass.STRM + '&class=' + (this.realClass.CLASS_NBR2 == '0' || !this.realClass.CLASS_NBR2?this.realClass.CLASS_NBR:this.realClass.CLASS_NBR2) + '&emplid=' + emplid, '_self');
+		if(this.realClass["STRM"] == '1154' || this.realClass["STRM"] == '1032'){
+			window.open(this.virtualRoom["NEWSTRM"] + 'local/wseducad/auth/sso.php?strm=' + this.realClass.STRM + '&class=' + (this.realClass.CLASS_NBR2 == '0' || !this.realClass.CLASS_NBR2?this.realClass.CLASS_NBR:this.realClass.CLASS_NBR2) + '&emplid=' + emplid, '_self');
+		} else {
+			window.open(this.virtualRoom[this.realClass.INSTITUTION] + 'local/wseducad/auth/sso.php?strm=' + this.realClass.STRM + '&class=' + (this.realClass.CLASS_NBR2 == '0' || !this.realClass.CLASS_NBR2?this.realClass.CLASS_NBR:this.realClass.CLASS_NBR2) + '&emplid=' + emplid, '_self');
+		}
 	}
 
 	closeOpenMonthViewDay(){
