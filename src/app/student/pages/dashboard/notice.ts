5524ba88a70e38e1f96ff82eb84f0c956f589a31
Edmundo <eacostac@cientifica.edu.pe>
2021-09-01 11:26:14 -0500

    notices

--- src/app/student/pages/dashboard/notice.ts
+++ src/app/student/pages/dashboard/notice.ts
@@ -14,7 +14,7 @@ export const notice = [
         imgPath: './assets/img/notice_2.jpg',
         title: 'Conéctate a clases fácil y rápido',
         new: false,
-        content: 'Ahora puedes conectarte más rápido a tus clases por Zoom desde la página de Inicio. Para ingresar, dale clic a “Clase en vivo”, sigue las indicaciones y listo. <br>⏰Recuerda: El botón se activa 10 minutos antes de iniciar la clase.',
+        content: 'Ahora puedes conectarte más rápido a tus clases por Zoom desde la página de Inicio. Para ingresar, dale clic a “Clase en vivo”, sigue las indicaciones y listo.  ⏰Recuerda: El botón se activa 10 minutos antes de iniciar la clase.',
         limit: (content)=>{
             return content.length>500? content.substring(0,500)+'...': content; 
         },
