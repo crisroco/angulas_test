f746884da5a70852a4149aa77f74f169a856ade3
Edmundo <eacostac@cientifica.edu.pe>
2021-09-08 17:35:09 -0500

    adding classes

--- src/app/student/pages/dashboard/notice.ts
+++ src/app/student/pages/dashboard/notice.ts
@@ -1,26 +1,26 @@
 export const notice = [
-    {
-        imgPath: './assets/img/notice_3.jpg',
-        title: 'Participa en las Actividades extracurriculares 2021 2 de VGU',
-        new: true,
-        content: 'En el semestre 2021 2, Vida y Gestión Universitaria estará desarrollando Actividades extracurriculares que contribuyen al desarrollo y formación integral de los estudiantes, promoviendo estilos de vida saludables y favoreciendo el contacto de la comunidad universitaria con el arte, la cultura, y espacios de sana distensión. <a href="https://drive.google.com/file/d/1eg5BCiFR8Coe4KfCu2vXVR1lwvVvK8Q9/view?usp=sharing">Más info aquí.</a>',
-        limit: (content)=>{
-            return content.length>500? content.substring(0,500)+'...': content; 
-        },
-        filtroInst: ['CPE','PREG'],
-        expand: true
-    },
-    {
-        imgPath: './assets/img/notice_2.jpg',
-        title: 'Conéctate a clases fácil y rápido',
-        new: false,
-        content: 'Ahora puedes conectarte más rápido a tus clases por Zoom desde la página de Inicio. Para ingresar, dale clic a “Clase en vivo”, sigue las indicaciones y listo.  ⏰Recuerda: El botón se activa 10 minutos antes de iniciar la clase.',
-        limit: (content)=>{
-            return content.length>500? content.substring(0,500)+'...': content; 
-        },
-        filtroInst: ['CPE','PREG'],
-        expand: true
-    },
+    // {
+    //     imgPath: './assets/img/notice_3.jpg',
+    //     title: 'Participa en las Actividades extracurriculares 2021 2 de VGU',
+    //     new: true,
+    //     content: 'En el semestre 2021 2, Vida y Gestión Universitaria estará desarrollando Actividades extracurriculares que contribuyen al desarrollo y formación integral de los estudiantes, promoviendo estilos de vida saludables y favoreciendo el contacto de la comunidad universitaria con el arte, la cultura, y espacios de sana distensión. <a href="https://drive.google.com/file/d/1eg5BCiFR8Coe4KfCu2vXVR1lwvVvK8Q9/view?usp=sharing">Más info aquí.</a>',
+    //     limit: (content)=>{
+    //         return content.length>500? content.substring(0,500)+'...': content; 
+    //     },
+    //     filtroInst: ['CPE','PREG'],
+    //     expand: true
+    // },
+    // {
+    //     imgPath: './assets/img/notice_2.jpg',
+    //     title: 'Conéctate a clases fácil y rápido',
+    //     new: false,
+    //     content: 'Ahora puedes conectarte más rápido a tus clases por Zoom desde la página de Inicio. Para ingresar, dale clic a “Clase en vivo”, sigue las indicaciones y listo.  ⏰Recuerda: El botón se activa 10 minutos antes de iniciar la clase.',
+    //     limit: (content)=>{
+    //         return content.length>500? content.substring(0,500)+'...': content; 
+    //     },
+    //     filtroInst: ['CPE','PREG'],
+    //     expand: true
+    // },
     {
         imgPath: 'https://us.123rf.com/450wm/antonioguillem/antonioguillem2004/antonioguillem200400060/144517200-close-up-of-woman-hand-filling-out-form-with-pen-on-a-desk.jpg?ver=6',
         title: 'Proyecto de investigación de UCSUR',
@@ -31,7 +31,7 @@ export const notice = [
         },
         filtroInst: ['PREG'],
         expand: true
-    },
+    }
     // {
     //     imgPath: './assets/img/pre-matricula.svg',
     //     title: 'Mira el Calendario de Pre-Matrícula',
