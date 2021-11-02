0161f4fae68203324f38e21b0f0d6d7980912b80
Edmundo <eacostac@cientifica.edu.pe>
2021-11-02 17:46:54 -0500

    adding phrases

--- src/app/student/pages/dashboard/phrases.ts
+++ src/app/student/pages/dashboard/phrases.ts
@@ -6,5 +6,45 @@ export const phrases = [
     {
         text: 'Si puedes soñarlo, puedes hacerlo',
         author: 'Walt Disney',
+    },
+    {
+        text: 'Los obstáculos son esas cosas que atemorizantes que ves cuando apartas los ojos de tu meta',
+        author: 'Henry Ford',
+    },
+    {
+        text: 'La felicidad no ocurre por casualidad, sino por elección',
+        author: 'Jim Rohn',
+    },
+    {
+        text: 'No es verdad que las personas paren de perseguir sus sueños porque se hacen viejos. Se hacen viejos porque dejan de perseguir sus sueños',
+        author: 'Gabriel García Márquez',
+    },
+    {
+        text: 'Lo que importa no es lo que ocurre, sin cómo reaccionas a ello',
+        author: 'Epicteto',
+    },
+    {
+        text: 'Sé el cambio que quieres ver en el mundo',
+        author: 'Mahatma Gandhi',
+    },
+    {
+        text: 'Nadie puede hacerte sentir inferior sin tu consentimiento',
+        author: 'Eleonor Roosevelt',
+    },
+    {
+        text: 'No es la muerte lo que debes temer, sino nunca empezar a vivir',
+        author: 'Marco Aurelio',
+    },
+    {
+        text: 'El secreto para salir adelante es comenzar',
+        author: 'Mark Twain',
+    },
+    {
+        text: 'La gota rompe la roca, no por su fuerza sino por su constancia',
+        author: 'Ovidio',
+    },
+    {
+        text: 'Tanto si crees que puedes como si crees que no puedes, tienes razón',
+        author: 'Henry Ford',
     }
 ];
