66303399767e19c7a32995d553ee8ab426a193aa
Edmundo <eacostac@cientifica.edu.pe>
2022-01-04 16:21:23 -0500

    view formalities

--- src/app/student/pages/action/charge-form/charge-form.component.ts
+++ src/app/student/pages/action/charge-form/charge-form.component.ts
@@ -12,15 +12,38 @@ export class ChargeFormComponent implements OnInit {
   user: any = this.session.getObject('user');
 	student: any = this.session.getObject('student');
   loading = false;
+  allTypes:Array<any> = [];
+  public charge = {
+    grade: '',
+    type: '',
+    payment: 'COSTO'
+  }
   constructor(private session: SessionService,
 		public toastS: ToastrService,
 		private studentS: StudentService) { }
 
   ngOnInit() {
-    this.studentS.getChargeTypes('PREGRADO')
+  }
+
+  loadTypes(type){
+    if(type != ''){
+      this.loading = true;
+      this.studentS.getChargeTypes(type)
       .then((res) => {
-        console.log(res);
-      })
+        this.allTypes = res;
+        this.loading = false;
+      });
+    } else {
+      this.toastS.error('Debes seleccionar un Grado');
+    }
+  }
+
+  createCharge(){
+    if(this.charge.grade == '' || this.charge.type){
+      this.toastS.warning('Falta seleccionar datos');
+    } else {
+
+    }
   }
 
 }
